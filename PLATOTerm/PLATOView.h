ab67c4cbd6c084eb42d44e14b4c7f4d258109e35
Gregory John Casamento <greg.casamento@gmail.com>
2019-04-17 08:18:29 -0400

    Add files

---
+++ PLATOTerm/PLATOView.h
@@ -0,0 +1,17 @@
+//
+//  PLATOView.h
+//  PLATOTerm
+//
+//  Created by Gregory John Casamento on 4/17/19.
+//  Copyright © 2019 Gregory John Casamento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PLATOView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PLATOTerm/PLATOView.m
@@ -0,0 +1,21 @@
+//
+//  PLATOView.m
+//  PLATOTerm
+//
+//  Created by Gregory John Casamento on 4/17/19.
+//  Copyright © 2019 Gregory John Casamento. All rights reserved.
+//
+
+#import "PLATOView.h"
+
+@implementation PLATOView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
